c8d63fbd95f3041a2a701d4fa6fa501bde07df7c
kevmasajedi <87088715+kevmasajedi@users.noreply.github.com>
2024-09-21 19:44:25 +0330

    V0.15 BC

--- templates/templates.go
+++ templates/templates.go
@@ -3,6 +3,8 @@ package templates
 import (
 	"encoding/json"
 	"html/template"
+	"math/rand"
+	"strconv"
 )
 
 func GetTemplateFunctions() template.FuncMap {
@@ -16,6 +18,24 @@ func GetTemplateFunctions() template.FuncMap {
 				return nil
 			}
 			return result
+		},
+		"html": func(data string) template.HTML {
+			return template.HTML(data)
+		},
+		"enumerator": func(base string, start byte, end byte) []string {
+			var result []string
+			for i := start; i <= end; i++ {
+				result = append(result, base+strconv.Itoa(int(i)))
+			}
+			return result
+		},
+		"uid": func() string {
+			const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+			b := make([]byte, 8)
+			for i := range b {
+				b[i] = charset[rand.Intn(len(charset))]
+			}
+			return string(b)
 		},
 	}
 }
