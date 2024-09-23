5e497832ab9f57257749742a8258c99fd6109cdf
kevmasajedi <87088715+kevmasajedi@users.noreply.github.com>
2024-09-23 18:10:35 +0330

    V0.2

--- workers/adapters/adapters.go
+++ workers/adapters/adapters.go
@@ -1,5 +1,9 @@
 package adapter
 
+import (
+	"event_horizon/system/hub"
+)
+
 func stack_contains_all_conditions(stack map[string]struct{}, conditions []string) bool {
 	for _, trigger := range conditions {
 		if _, found := stack[trigger]; !found {
@@ -13,25 +17,25 @@ func remove_items_from_stack(stack map[string]struct{}, items []string) {
 		delete(stack, item)
 	}
 }
-func MeetAllConditions(downlink chan string, uplink chan string, loglink chan string, conditions []string, emission string) {
+func MeetAllConditions(hub *hub.Hub, conditions []string, emission string) {
 	stack := make(map[string]struct{})
-	for msg := range downlink {
+	for msg := range hub.DownLink() {
 		stack[msg] = struct{}{}
 		if stack_contains_all_conditions(stack, conditions) {
 			for _, c := range conditions {
-				loglink <- c + "->" + emission
+				hub.LogLink() <- c + "->" + emission
 			}
-			uplink <- emission
+			hub.UpLink() <- emission
 			remove_items_from_stack(stack, conditions)
 		}
 	}
 }
-func MeetSomeConditions(downlink chan string, uplink chan string, loglink chan string, conditions []string, emission string) {
-	for msg := range downlink {
+func MeetSomeConditions(hub *hub.Hub, conditions []string, emission string) {
+	for msg := range hub.DownLink() {
 		for _, c := range conditions {
 			if msg == c {
-				loglink <- c + "->" + emission
-				uplink <- emission
+				hub.LogLink() <- c + "->" + emission
+				hub.UpLink() <- emission
 				break
 			}
 		}
